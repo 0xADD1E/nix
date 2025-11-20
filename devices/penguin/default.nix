06ae08ba9be85b3643f6bdfb9df255734aa6e44a
Karolína Lišková <developer@0xadd1e.me>
2025-11-20 15:13:31 +0000

    Update default.nix

--- devices/penguin/default.nix
+++ devices/penguin/default.nix
@@ -8,6 +8,9 @@
       "${myModulesRoot}/nixos-baseline"
     ];
   home-manager-custom.homeModuleFlags = [ "linux" ];
+  environment.variables = {
+    NIX_REMOTE="daemon";
+  };
   security.sudo = {
     extraRules = [{
       commands = [
