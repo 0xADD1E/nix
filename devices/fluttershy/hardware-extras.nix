df7157b65163c4877a06b51d037dafb401d07a0d
Karolina Liskova <developer@0xADD1E.me>
2026-05-19 10:35:04 +0100

    Add fix script for "Touchpad not working after suspend"

--- devices/fluttershy/hardware-extras.nix
+++ devices/fluttershy/hardware-extras.nix
@@ -1,4 +1,4 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
   services.hardware.bolt.enable = true;
   services.beesd.filesystems = {
     "root" = {
@@ -7,4 +7,14 @@
       extraOptions = let loadTargetFract = 0.25; in [ "--loadavg-target" "${lib.strings.floatToString (loadTargetFract*16)}" ];
     };
   };
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "fix-touchpad-suspend" ''
+      if [ "$USER" != "root" ]; then
+        echo "Uses rmmod and modprobe; must be run as root"
+        exit 1
+      fi
+      ${pkgs.kmod}/bin/rmmod i2c_hid_acpi
+      ${pkgs.kmod}/bin/modprobe i2c_hid_acpi
+    '')
+  ];
 }
