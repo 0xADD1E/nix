561ddd741dda808b1082a67b3fc91e1b463bddc5
Karolina Liskova <developer@0xADD1E.me>
2026-06-15 11:42:15 +0100

    Boots

---
+++ modules/nixos-microvm/default.nix
@@ -0,0 +1,43 @@
+{ inputs, pkgs, unstablePkgs, myModulesRoot, myHomesRoot, ... }: {
+  imports = [ inputs.microvm.nixosModules.host ];
+  microvm.vms = {
+    clanker-sandbox = {
+      inherit pkgs;
+      config = {
+        microvm = {
+          #graphics.enable=true;
+          hypervisor = "cloud-hypervisor";
+          hotplugMem = 8 * 1024; #8GB
+          vsock.cid = 3;
+          shares = [{
+            source = "/nix/store";
+            mountPoint = "/nix/.ro-store";
+            tag = "ro-store";
+            proto = "virtiofs";
+            readOnly = true;
+          }];
+        };
+        #  imports = [ 
+        #"${myModulesRoot}/nixos-baseline"
+        #];
+        #home-manager-custom = {
+        #    homeModuleFlags = [ "linux" ];
+        #    enabledUsers = [ "kaja" ];
+        #  };
+        environment.systemPackages = [
+          pkgs.opencode
+          #pkgs.pi-coding-agent
+        ];
+        environment.sessionVariables = {
+          WAYLAND_DISPLAY = "wayland-1";
+          DISPLAY = ":0";
+          QT_QPA_PLATFORM = "wayland"; # Qt Applications
+          GDK_BACKEND = "wayland"; # GTK Applications
+          XDG_SESSION_TYPE = "wayland"; # Electron Applications
+          SDL_VIDEODRIVER = "wayland";
+          CLUTTER_BACKEND = "wayland";
+        };
+      };
+    };
+  };
+}
